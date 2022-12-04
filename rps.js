589dae39ca64814ffe730396bd873541ff4dbeac
schaulet207 <shannon.chaulet@gmail.com>
2022-12-03 21:54:55 -0500

    Build Framework

--- rps.js
+++ rps.js
@@ -1,5 +1,7 @@
 // Randomly returns either ‘Rock’, ‘Paper’ or ‘Scissors’ for the variable computerChoice
 
+let playerSelection = null;
+
 function getComputerChoice() {
 
     let randNum = Math.floor(Math.random() * 3);
@@ -7,70 +9,83 @@ function getComputerChoice() {
         (randNum === 1) ? 'Paper' :
         'Scissors';
 }
-getComputerChoice()
+getComputerChoice();
 
-// Returns the string with only the first letter capitalized. 
-function capitalize(playerSelection) {
-    let lower = playerSelection.slice(0, 1).toUpperCase();
-    let upper = playerSelection.slice(1).toLowerCase();
-    playerSelectionCapitalized = (lower + upper);
-}
-
-// Set wins, loss and tie variables to 0
+// Set wins, loss, tie and winner variables to 0
 let wins = 0;
 let losses = 0;
 let ties = 0;
 
-// Play a single round of RPS
-    function playRound() {
-        var playerSelection = prompt("Select One: Rock, Paper or Scissors?");
-            playerSelection = playerSelection.toLowerCase();
-            capitalize(playerSelection);
-        if (playerSelectionCapitalized === 'Rock' && computerChoice === 'Rock') {
-            ties++;
-            return "It's a tie!";
-        } else if (playerSelectionCapitalized === 'Paper' && computerChoice === 'Paper') {
-            ties++;
-            return "It's a tie!";
-        } else if (playerSelectionCapitalized === 'Scissors' && computerChoice === 'Scissors') {
-            ties++;
-            return "It's a tie!";
-        } else if (playerSelectionCapitalized === 'Rock' && computerChoice === 'Scissors') {
-            wins++;
-            return "You win! Rock beats Scissors!";
-        } else if (playerSelectionCapitalized === 'Paper' && computerChoice === 'Rock') {
-            wins++;
-            return "You win! Paper beats Rock!";
-        } else if (playerSelectionCapitalized === 'Scissors' && computerChoice === 'Paper') {
-            wins++;
-            return "You win! Scissors beats Paper!";
-        } else if (playerSelectionCapitalized === 'Rock' && computerChoice === 'Paper') {
-            losses++;
-            return "You lost! Rock beats Paper!";
-        } else if (playerSelectionCapitalized === 'Paper' && computerChoice === 'Scissors') {
-            losses++;
-            return "You lost! Paper beats Scissors!";
-        } else if (playerSelectionCapitalized === 'Scissors' && computerChoice === 'Rock') {
-            losses++;
-            return "You lost! Scissors beats Rock!";
-        } else {
-            alert('Invalid input');
-        }
-}
-
 // Play five rounds of RPS and declare a winner
-function game() {
-    for (let i = 0; i < 5; i++){
-        playRound();
-    } if (wins > losses) {
-            alert('You won!');
+function game(wins, losses) {
+    if (wins === 5) {
+            document.getElementById("winner").innerHTML = "YOU WON!";
         }
-        else if (losses > wins) {
-            alert('Computer won!');
-        }
-        else {
-            alert("It's a tie!");
+    else if (losses === 5) {
+            document.getElementById("winner").innerHTML = "YOU LOST!";
         }
     }
 
-game();
+// Play a single round of RPS
+    function playRound() {
+        getComputerChoice();
+        if (playerSelection === 'Rock' && computerChoice === 'Rock') {
+            ties++;
+            alert("It's a tie!");
+        } else if (playerSelection === 'Paper' && computerChoice === 'Paper') {
+            ties++;
+            alert("It's a tie!");
+        } else if (playerSelection === 'Scissors' && computerChoice === 'Scissors') {
+            ties++;
+            alert("It's a tie!");
+        } else if (playerSelection === 'Rock' && computerChoice === 'Scissors') {
+            wins++;
+            alert("You win! Your rock beats computers Scissors!");
+        } else if (playerSelection === 'Paper' && computerChoice === 'Rock') {
+            wins++;
+            alert("You win! Your paper beats computers rock!");
+        } else if (playerSelection === 'Scissors' && computerChoice === 'Paper') {
+            wins++;
+            alert("You win! Your scissors beats computers paper!");
+        } else if (playerSelection === 'Rock' && computerChoice === 'Paper') {
+            losses++;
+            alert("You lost! Computers paper beats your rock!");
+        } else if (playerSelection === 'Paper' && computerChoice === 'Scissors') {
+            losses++;
+            alert("You lost! Computers scissors beats your paper!");
+        } else if (playerSelection === 'Scissors' && computerChoice === 'Rock') {
+            losses++;
+            alert("You lost! Computers rock beats your scissors!");
+        } else {
+            alert('Invalid input');
+        }
+        console.log(computerChoice, playerSelection);
+        document.getElementById("wins").innerHTML = wins;
+        document.getElementById("losses").innerHTML = losses;
+        document.getElementById("ties").innerHTML = ties;
+}
+
+rock.addEventListener('click', function() {
+    console.log("Clicked Rock");
+    playerSelection = "Rock";
+    playRound("Rock");
+    game(wins, losses)
+  });
+
+paper.addEventListener('click', function() {
+    console.log("Clicked Paper");
+    playerSelection = "Paper";
+    playRound("Paper");
+  });
+
+scissors.addEventListener('click', function() {
+    console.log("Clicked Scissors");
+    playerSelection = "Scissors";
+    playRound("Scissors");
+  });
+
+// Displays current score
+document.getElementById("wins").innerHTML = wins;
+document.getElementById("losses").innerHTML = losses;
+document.getElementById("ties").innerHTML = ties;
+document.getElementById("winner").innerHTML = "First to five wins!";
